178fcee8d9c29d2c5349696981a214f5eac9cb28
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-10-20 14:27:57 +0100

    add font awesome library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
-<template>
-    <div>
-        <h2 class="text-red-500 text-center text-2xl font-bold">Hello world</h2>
-    </div>
-</template>
-
 <script>
 
 </script>
 
-<style scoped>
-
-</style>
+<template>
+    <nav class="container py-2 border border-white text-white flex justify-between items-center">
+        <div class="flex items-center gap-10">
+            <img class="w-14 h-14" src="/images/Movie-House-logo.png" alt="logo">
+            <ul class="flex gap-3">
+                <li class="text-2xl">Home</li>
+                <li class="text-2xl">My Movies</li>
+            </ul>
+        </div>
+        <i class="fa-solid fa-magnifying-glass hover:text-primary cursor-pointer text-2xl"></i>
+    </nav>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2.25rem'
+    },
+    extend: {
+      colors: {
+        'primary': '#F68B1E'
+      },
+    },
   },
   plugins: [],
 }
