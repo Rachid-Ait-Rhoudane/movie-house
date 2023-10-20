dab99345979a4908eddc2a06c30bf51c9d2a76ef
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-10-20 14:40:43 +0100

    add navigation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,24 @@
 </script>
 
 <template>
-    <nav class="container py-2 border border-white text-white flex justify-between items-center">
+    <nav class="container py-2 text-white flex justify-between items-center">
         <div class="flex items-center gap-10">
-            <img class="w-14 h-14" src="/images/Movie-House-logo.png" alt="logo">
-            <ul class="flex gap-3">
-                <li class="text-2xl">Home</li>
-                <li class="text-2xl">My Movies</li>
+            <router-link to="/">
+                <img class="w-14 h-14" src="/images/Movie-House-logo.png" alt="logo">
+            </router-link>
+            <ul class="flex gap-12">
+                <li class="text-xl hover:text-gray-300">
+                    <router-link class="text-primary border-b-2 border-b-primary p-4" to="/">
+                        <i class="fa-solid fa-house"></i>
+                        Home
+                    </router-link>
+                </li>
+                <li class="text-xl hover:text-gray-300">
+                    <router-link class="p-4" to="/about">
+                        <i class="fa-solid fa-bookmark"></i>
+                        My Movies
+                    </router-link>
+                </li>
             </ul>
         </div>
         <i class="fa-solid fa-magnifying-glass hover:text-primary cursor-pointer text-2xl"></i>
