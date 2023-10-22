d835fba9e5111a7105a7664bb618a11271ce8dc1
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-10-22 13:28:50 +0100

    add vue awesome paginate

--- src/components/MostPopular.vue
+++ src/components/MostPopular.vue
@@ -7,6 +7,16 @@ export default {
     components: {
         SpecialHeader,
         MovieCard
+    },
+    data() {
+        return {
+            currentPage: 1
+        }
+    },
+    methods: {
+        onClickHandler(page) {
+            console.log(page);
+        }
     }
 }
 
@@ -25,6 +35,59 @@ export default {
             <movie-card />
             <movie-card />
         </div>
+        <vue-awesome-paginate
+            :total-items="50"
+            :items-per-page="5"
+            :max-pages-shown="5"
+            v-model="currentPage"
+            :on-click="onClickHandler"
+            :show-breakpoint-buttons="false"
+            :hide-prev-next-when-ends="true"
+        />
     </div>
-    
+
 </template>
+
+<style>
+
+.pagination-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    margin-top: 60px;
+    width: 100%;
+}
+
+.paginate-buttons {
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    cursor: pointer;
+    background-color: transparent;
+    border: 2px solid #F68B1E;
+    color: #F68B1E;
+}
+
+@media (max-width: 350px) {
+    .paginate-buttons {
+        height: 30px;
+        width: 30px;
+    }
+}
+
+.paginate-buttons:hover {
+    background-color: #181818;
+}
+
+.active-page {
+    background-color: #F68B1E;
+    border: 2px solid #F68B1E;
+    color: white;
+}
+
+.active-page:hover {
+    background-color: #F68B1E;
+}
+
+</style>
