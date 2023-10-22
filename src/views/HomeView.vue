754242df995ac2ed2c732b3d49a39beccb8966a2
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-10-22 12:13:07 +0100

    add most popular section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,8 +25,21 @@ export default {
     </div>
 
 
-    <div class="py-16">
+    <div class="py-16 mt-16">
         <special-header>Most Popular</special-header>
+        <div class="container mt-24 grid grid-cols-1 xsm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+            <movie-card />
+            <movie-card />
+            <movie-card />
+            <movie-card />
+            <movie-card />
+            <movie-card />
+            <movie-card />
+        </div>
+    </div>
+
+    <div class="py-16 mt-16">
+        <special-header>Movies</special-header>
         <div class="container mt-24 grid grid-cols-1 xsm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
             <movie-card />
             <movie-card />
