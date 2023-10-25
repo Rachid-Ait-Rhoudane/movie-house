cc895af78405032d25d866b28c04801aa22f0297
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-10-25 13:26:35 +0100

    add img loader

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,6 +1,21 @@
 <script>
 
+import ImgLoader from './ImgLoader.vue';
+
 export default {
+    components: {
+        ImgLoader
+    },
+    data() {
+        return {
+            showImg: false
+        }
+    },
+    methods: {
+        imgLoaded() {
+            this.showImg = true;
+        }
+    },
     props: {
         poster: {
             type: String,
@@ -24,7 +39,8 @@ export default {
         <span class="absolute z-20 top-0 left-2 after:absolute after:top-0 after:left-0 after:block after:border-x-[24px] after:border-t-[60px] after:border-b-[24px] after:border-secondary/60 after:border-b-transparent">
             <i class="fa-solid fa-plus text-xl text-white absolute z-30 left-4 top-4 cursor-pointer"></i>
         </span>
-        <img class="w-full aspect-[3/4]" :src="poster" alt="poster image">
+        <img v-show="showImg" @load="imgLoaded" class="w-full aspect-[3/4]" :src="poster" alt="poster image">
+        <img-loader v-show="!showImg" />
         <div class="p-2">
             <h1 class="text-xl capitalize font-bold text-white min-h-[55px] max-w-full line-clamp-2">{{ title }}</h1>
             <span class="block mt-2 text-white">
@@ -37,5 +53,6 @@ export default {
             <i class="fa-solid fa-right-long group-hover:animate-bounce-left-right text-primary text-xl"></i>
         </div>
     </div>
+
     
 </template>
