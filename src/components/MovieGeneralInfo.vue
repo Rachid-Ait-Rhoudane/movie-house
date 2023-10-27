3b722727a9e1c73819d97f6fed0d67db0f787e59
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-10-27 13:16:54 +0100

    add movie general info component

---
+++ src/components/MovieGeneralInfo.vue
@@ -0,0 +1,47 @@
+<script>
+
+    export default {
+        props: {
+            poster: {
+                type: String,
+                required: true
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            overview: {
+                type: String,
+                required: true
+            },
+            genres: {
+                type: Array,
+                required: true
+            },
+            rate: {
+                type: Number,
+                required: true
+            }
+        }
+    }
+
+</script>
+
+<template>
+
+    <div class="flex items-center flex-col sm:flex-row gap-4 w-full">
+        <img class="w-80 aspect-[3/4]" :src="`https://image.tmdb.org/t/p/original/${poster}`" alt="poster image">
+        <div class="flex flex-col gap-5 text-white">
+            <h1 class="text-3xl md:text-6xl font-bold">{{ title }}</h1>
+            <div class="flex items-center gap-2">
+                <span class="text-sm py-1 px-2 border border-white rounded-md" v-for="genre in genres" :key="genre.id">{{ genre.name }}</span>
+            </div>
+            <span class="flex items-center gap-2 text-base md:text-xl">
+                <i class="fa-solid fa-star text-primary"></i>
+                {{ rate }}
+            </span>
+            <p class="text-base md:text-xl">{{ overview }}</p>
+        </div>
+    </div>
+
+</template>
--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,12 +1,14 @@
 <script>
 
 import SpecialHeader from "../components/SpecialHeader.vue";
+import MovieGeneralInfo from "../components/MovieGeneralInfo.vue";
 import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 
 export default {
     components: {
         SpecialHeader,
+        MovieGeneralInfo,
         Swiper,
         SwiperSlide
     },
@@ -50,20 +52,13 @@ export default {
     <div v-if="movie" class="relative overflow-x-hidden w-screen h-screen before:fixed before:top-0 before:left-0 before:bg-black/80 before:w-full before:h-full before:z-10">
         <img class="fixed top-0 left-0 w-full h-full object-cover" :src="`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`" alt="movie backdrop">
         <div class="relative z-20 container mt-24">
-            <div class="flex items-center flex-col sm:flex-row gap-4 w-full">
-                <img class="w-80 aspect-[3/4]" :src="`https://image.tmdb.org/t/p/original/${movie.poster_path}`" alt="poster image">
-                <div class="flex flex-col gap-5 text-white">
-                    <h1 class="text-3xl md:text-6xl font-bold">{{ movie.title }}</h1>
-                    <div class="flex items-center gap-2">
-                        <span class="text-sm py-1 px-2 border border-white rounded-md" v-for="genre in movie.genres" :key="genre.id">{{ genre.name }}</span>
-                    </div>
-                    <span class="flex items-center gap-2 text-base md:text-xl">
-                        <i class="fa-solid fa-star text-primary"></i>
-                        {{ movie.vote_average }}
-                    </span>
-                    <p class="text-base md:text-xl">{{ movie.overview }}</p>
-                </div>
-            </div>
+            <movie-general-info 
+                :poster="movie.poster_path"
+                :title="movie.title"
+                :genres="movie.genres"
+                :rate="movie.vote_average"
+                :overview="movie.overview"
+            />
             <div class="my-20">
                 <h1 class="text-3xl md:text-6xl font-bold py-1 md:py-2 px-1 md:px-2 border-l-[5px] md:border-l-[10px] border-l-primary capitalize text-white">
                     Numbers
