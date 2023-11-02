1068ba90e510050334d9e62cbd017f50dc57ef78
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-11-02 14:36:08 +0100

    add image render component

---
+++ src/components/ImgRender.vue
@@ -0,0 +1,38 @@
+<script>
+
+import ImgLoader from './ImgLoader.vue';
+
+export default {
+    components: {
+        ImgLoader
+    },
+    data() {
+        return {
+            showImg: false
+        }
+    },
+    props: {
+        class: {
+            type: String
+        },
+        path: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        imgLoaded() {
+            this.showImg = true;
+        },
+        imageLoadOnError(e) {
+            e.target.src = "/images/poster-error.png";
+        },
+    }
+}
+
+</script>
+
+<template>
+    <img v-show="showImg" @load="imgLoaded" @error="imageLoadOnError" :class="class" :src="path" alt="image">
+    <img-loader :class="class" v-show="!showImg" />
+</template>
--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,20 +1,10 @@
 <script>
 
-import ImgLoader from './ImgLoader.vue';
+import ImgRender from './ImgRender.vue';
 
 export default {
     components: {
-        ImgLoader
-    },
-    data() {
-        return {
-            showImg: false
-        }
-    },
-    methods: {
-        imgLoaded() {
-            this.showImg = true;
-        }
+        ImgRender
     },
     props: {
         id: {
@@ -43,8 +33,7 @@ export default {
         <span class="absolute z-20 top-0 left-2 after:absolute after:top-0 after:left-0 after:block after:border-x-[24px] after:border-t-[60px] after:border-b-[24px] after:border-secondary/60 after:border-b-transparent">
             <i class="fa-solid fa-plus text-xl text-white absolute z-30 left-4 top-4 cursor-pointer"></i>
         </span>
-        <img v-show="showImg" @load="imgLoaded" class="w-full aspect-[3/4]" :src="poster" alt="poster image">
-        <img-loader class="w-full aspect-[3/4]" v-show="!showImg" />
+        <img-render :path="poster" class="w-full aspect-[3/4]" />
         <div class="p-2">
             <h1 class="text-xl capitalize font-bold text-white min-h-[55px] max-w-full line-clamp-2">{{ title }}</h1>
             <span class="block mt-2 text-white">
