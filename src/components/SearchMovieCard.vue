ff0958a4ff1233a4dbc5433505db9c76cc68d85b
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-11-02 13:17:04 +0100

    add search movie card component

---
+++ src/components/SearchMovieCard.vue
@@ -0,0 +1,57 @@
+<script>
+
+import ImgLoader from './ImgLoader.vue';
+
+export default {
+    components: {
+        ImgLoader
+    },
+    data() {
+        return {
+            showImg: false
+        }
+    },
+    methods: {
+        imgLoaded() {
+            this.showImg = true;
+        }
+    },
+    props: {
+        poster: {
+            type: String,
+            reuiqred: true
+        },
+        title: {
+            type: String,
+            reuiqred: true
+        },
+        rate: {
+            type: Number,
+            reuiqred: true
+        },
+    }
+}
+
+</script>
+
+<template>
+    
+    <div  class="relative flex gap-2 text-white border border-white bg-secondary">
+        <img v-show="showImg" @load="imgLoaded" class="w-24 aspect-[3/4]" :src="`https://image.tmdb.org/t/p/original${poster}`" alt="movie poster">
+        <img-loader class="w-24 aspect-[3/4]" v-show="!showImg" />
+        <div class="flex flex-col sm:flex-row items-start sm:items-center justify-between flex-1 p-2 gap-2">
+            <div class="flex flex-col gap-2 w-3/4">
+                <span class="text-base sm:text-xl font-bold line-clamp-2">{{ title }}</span>
+                <span>
+                    <i class="fa-solid fa-star text-primary"></i>
+                    {{ rate }}
+                </span>
+            </div>
+            <button class="text-white self-center py-2 px-4 border border-white rounded-md hover:bg-primary">
+                <i class="fa-solid fa-bookmark"></i>
+                Save
+            </button>
+        </div>
+    </div>
+
+</template>
