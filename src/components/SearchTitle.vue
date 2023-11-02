a2dd4398711a9afcef487d50306d627f740ee45f
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2023-11-02 12:50:57 +0100

    add search title component

---
+++ src/components/SearchTitle.vue
@@ -0,0 +1,44 @@
+<script>
+
+import SearchResults from './SearchResults.vue';
+
+export default {
+    components: {
+        SearchResults
+    },
+    data() {
+        return {
+            title: '',
+            results: []
+        }
+    },
+    methods: {
+        getResults() {
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjI3MTdlOTU1ZmU3M2Y1OTkxNGU4NDU3ZWRiZGU1MyIsInN1YiI6IjY1MzNiOGMwOGNmY2M3MDBjODNkNjVjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VCBH0D_Q6oZ5JcoXZyIrCFbWIpOSxRFL54ZefY6koXs'
+                }
+            };
+
+            fetch(`https://api.themoviedb.org/3/search/movie?query=${this.title}`, options)
+            .then(response => response.json())
+            .then(response => this.results = response.results)
+            .catch(err => console.error(err));
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <div class="relative mt-4 flex items-center">
+        <i class="fa-solid fa-magnifying-glass text-base md:text-xl text-white absolute"></i>
+        <input v-model="title" @input="getResults" class="bg-black/60 border-b border-b-white focus:outline-none text-white h-10 w-full pl-7" type="text" placeholder="Enter a movie name">
+    </div>
+
+    <search-results :show="title.length" :results="results"  />
+
+</template>
